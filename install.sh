b67b8a7d8e2e3bc5adfaea72ec927d14f71e05d9
liuxd <liuxidong1984@gmail.com>
2014-03-10 22:48:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-#设置git相关内容的彩色显示
+#colorful
 git config --global color.status auto
 git config --global color.diff auto
 git config --global color.branch auto
 git config --global color.interactive auto
 
-#别名配置
+#aliases
 git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.df diff
 git config --global alias.co checkout
 git config --global alias.br status
 
-#拷贝必要文件
+#copy
 cp .git-completion.bash ~/
 cp .git_aliases ~/
 cp -r git-ps1 ~/
 
-#配置信息
+#configure
 cat .bash_profile >> ~/.bash_profile
