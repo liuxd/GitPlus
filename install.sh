3c02e335d360818f89535438e4ecdcb978b1ad30
liuxd <liuxidong1984@gmail.com>
2014-04-08 15:07:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-#colorful
-git config --global color.status auto
-git config --global color.diff auto
-git config --global color.branch auto
-git config --global color.interactive auto
-
-#aliases
-git config --global alias.st status
-git config --global alias.ci commit
-git config --global alias.df diff
-git config --global alias.co checkout
-git config --global alias.br status
-
 #copy
 cp .git-completion.bash ~/
 cp .git_aliases ~/
